967a9c30813d3398b9f0f339b50fc6c196f3ab67
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-02-18 13:34:08 -0500

    Reapply "Merge branch 'main' of https://github.com/popoyo07/GP2_Hallway"
    This reverts commit a04bf74d0fe0d2ea2ebd9e760561bf04d270dd73.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Slider staminaSlider;
     public float speed = 5f;
     public float sprintSpeed = 10f;
     public float proneSpeed = 2.5f;
@@ -17,6 +19,13 @@ public class Player : MonoBehaviour
     public float proneCamHeight = -0.5f;
     private Vector3 originalCenter;
     private bool isSprinting = false;
+    public float maxStamina = 60f;
+    public float currentStamina;
+    public float staminaDrain = 20f;
+    public float staminaRegenSpeed = 10f;
+    public float staminaDelay = 2f;
+    private float recentSprint;
+
    
 
     // Start is called before the first frame update
@@ -29,6 +38,8 @@ public class Player : MonoBehaviour
         Vector3 cameraPosition = cam.localPosition;
         cameraPosition.y = standingCamHeight;
         cam.localPosition = cameraPosition;
+
+        currentStamina = maxStamina;
     }
 
     // Update is called once per frame
@@ -36,6 +47,11 @@ public class Player : MonoBehaviour
     {
         Movement();
         Crouching();
+
+        if (staminaSlider != null)
+        {
+            staminaSlider.value = currentStamina;
+        }
     }
 
     void Movement()
@@ -46,14 +62,17 @@ public class Player : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         if(isProne == false)
         {
-            if(isSprinting == false)
-            {
-                controller.Move(move * speed * Time.deltaTime);
-            }
-            
-            else if (isSprinting == true)
+            if (isSprinting == true && currentStamina > 0)
             {
                 controller.Move(move * sprintSpeed * Time.deltaTime);
+                currentStamina -= staminaDrain * Time.deltaTime;
+                recentSprint = Time.time;
+               
+            }
+            else
+            {
+                isSprinting = false;
+                controller.Move(move * speed * Time.deltaTime);
             }
         }
         else if (isProne == true)
@@ -62,7 +81,7 @@ public class Player : MonoBehaviour
         }
 
 
-        if(Input.GetKeyDown(KeyCode.LeftShift) && isProne == false)
+        if(Input.GetKeyDown(KeyCode.LeftShift) && isProne == false && currentStamina > 0)
         {
             isSprinting = true;
         }
@@ -71,6 +90,8 @@ public class Player : MonoBehaviour
         {
             isSprinting = false;
         }
+
+        StaminaRegen();
     }
 
     void Crouching()
@@ -91,5 +112,17 @@ public class Player : MonoBehaviour
         Vector3 cameraPosition = cam.localPosition;
         cameraPosition.y = targetCamHeight;
         cam.localPosition = cameraPosition;
+    }
+
+    void StaminaRegen()
+    {
+        if(isSprinting == false && currentStamina < maxStamina && Time.time > recentSprint + staminaDelay)
+        {
+            currentStamina += staminaRegenSpeed * Time.deltaTime;
+
+            //Limit stamina
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+            
+        }
     }
 }
